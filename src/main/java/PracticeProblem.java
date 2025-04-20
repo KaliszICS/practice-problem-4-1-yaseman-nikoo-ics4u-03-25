3c58c4bdb9dabe2257302974a17e36c38c510d2d
yaseman-nikoo-ics4u-03-25 <yaseman.nikoo@student.tdsb.on.ca>
2025-04-20 14:00:36 +0000

    modify

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -2,26 +2,45 @@ public class PracticeProblem {
 
 	public static void main(String args[]) {
 
+
 	}
 
-	public static void q1() {
-		//Write question 1 code here
-	}
-
-	public static void q2() {
-		//Write question 2 code here
-	}
-
-	public static void q3() {
-		//Write question 3 code here
-	}
-
-	public static void q4() {
-		//Write question 4 code here
-	}
-
-	public static void q5() {
-		//Write question 5 code here
-	}
 
 }
+class Rectangle{
+	private double length;
+	private double width;
+	public Rectangle(double length, double width){
+		this.length= length;
+		this.width= width;
+	}
+	public double getLength(){
+		return this.length;
+	}
+	public double getWidth() {
+		return this.width;
+	}
+	public double area(){
+		return this.length*this.width;
+	}
+	public double perimeter(){
+		return (this.length+this.width)*2;
+	}
+}
+class Circle{
+	private double radius;
+	public Circle(double radiud){
+		this.radius=radius;
+
+	}
+	public double getRadius(){
+		return this.radius;
+	}
+	public double area(){
+		return 3.14*Math.pow(this.radius,2);
+	}
+	public double circumference(){
+		return 3.14*2*this.radius;
+	}
+}
+
